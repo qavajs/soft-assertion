fa88b846c05fab6ba7885f87e11bbe778c58d9d7
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-01-28 12:30:24 +0200

    introduced new approach to perform soft assertions

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,33 @@
 import TestCaseRunner from '@cucumber/cucumber/lib/runtime/test_case_runner';
-import {
-    PickleTag,
-    TestStep,
-    TestStepResult,
-    TestStepResultStatus,
-    StepKeywordType,
-    PickleStep
-} from '@cucumber/messages';
+import { TestStepResultStatus } from '@cucumber/messages';
+import { BeforeStep, Then } from '@cucumber/cucumber';
 
-type TestStepResultType = {
-    status: string,
-    type: string
-};
-
-declare global {
-    // eslint-disable-next-line no-var
-    var config: { softAssertTag?: string };
+type ExtendedTestRunner = Partial<TestCaseRunner> & {
+    testStepResults: any[];
 }
 
-const originIsSkippingSteps = TestCaseRunner.prototype.isSkippingSteps;
-// patch isSkippingStep method
-TestCaseRunner.prototype.isSkippingSteps = function (this: TestCaseRunner) {
-    // @ts-ignore
-    const { pickle, testStepResults, testCase, currentTestStepId } = this;
-
-    const activationTag = global?.config?.softAssertTag ?? '@softAssert';
-    const isSoftAssertEnabled = pickle.tags.find((tag: PickleTag) => tag.name === activationTag);
-    if (!isSoftAssertEnabled) {
-        return originIsSkippingSteps.apply(this);
-    }
-
-    const testStepResultsTypes = testStepResults.map((result: TestStepResult, i: number): TestStepResultType => {
-        const testStep = testCase.testSteps[i];
-        const pickleStep = pickle.steps.find((step: TestStep) => step.id === testStep?.pickleStepId);
-        return { status: result.status, type: pickleStep?.type }
+TestCaseRunner.prototype.isSkippingSteps = function (this: ExtendedTestRunner) {
+    return this.testStepResults.some(stepResult => {
+        return stepResult.status !== TestStepResultStatus.PASSED
+          && !!stepResult.exception
+          && stepResult.exception.type !== 'SoftAssertionError'
     });
+}
 
-    const isNotOutcomeFailed = testStepResultsTypes.find((result: TestStepResultType) =>
-        result.status !== TestStepResultStatus.PASSED && result.type !== StepKeywordType.OUTCOME
-    );
-    const currentTestStep = testCase.testSteps.find((step: TestStep) => step.id === currentTestStepId);
-    const currentPickleStep = pickle.steps.find((step: PickleStep) => step.id === currentTestStep?.pickleStepId);
-    const isCurrentStepOutcome = currentPickleStep?.type === StepKeywordType.OUTCOME;
-    if (isCurrentStepOutcome && !isNotOutcomeFailed) {
-        return false
+class SoftAssertionError extends Error {
+    name = 'SoftAssertionError';
+}
+
+let currentStep: string;
+BeforeStep(async function(data) {
+    currentStep = data.pickleStep.text;
+});
+
+Then(/softly expect/, async function() {
+    const stepText = currentStep.replace('softly expect', 'expect');
+    try {
+        await this.executeStep(stepText);
+    } catch (err: any) {
+        throw new SoftAssertionError(err.message);
     }
-    return originIsSkippingSteps.apply(this)
-};
+});
--- test-e2e/step_definitions/custom.ts
+++ test-e2e/step_definitions/custom.ts
@@ -1,4 +1,4 @@
-import { Then } from '@cucumber/cucumber';
+import { BeforeStep, Then } from "@cucumber/cucumber";
 import { equal } from 'node:assert';
 
 Then('I expect {string} to equal {string}', function (left: string, right: string) {
